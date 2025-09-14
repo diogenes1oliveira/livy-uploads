de8d81bc0d56c6111f270f3940f8795639e66222
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2025-09-14 09:08:19 -0300

    feat: simpler console read

--- livy_uploads/executor/console.py
+++ livy_uploads/executor/console.py
@@ -1,53 +1,40 @@
 from abc import ABC, abstractmethod
-from datetime import datetime, timezone
 import os
 import select
 import signal
 import struct
 import fcntl
-import queue
-import time
 import termios
 import sys
-import tty
-from typing import BinaryIO, NamedTuple, Optional, Tuple, Union
+from typing import BinaryIO, Optional, Tuple
 
-
-class Signal(NamedTuple):
-    signum: Union[int, signal.Signals]
-    'The signal number'
-    tty_size: Optional[Tuple[int, int]]
-    'The size of the TTY if available'
-    t0: float
-    'The monotonic timestamp when the signal was received'
-    timestamp: datetime
-    'The UTC timestamp when the signal was received'
 
 
 class Console(ABC):
     """
-    A class to abstract the stdin and signals of a terminal process.
+    A class to abstract interruptibly reading from the stdin.
     """
 
-    def __init__(self, pause: Optional[float] = None) -> None:
-        self.pause = pause or 1.0
-        self.signals_queue = queue.Queue()
+    def __init__(self, stdin: Optional[BinaryIO] = None, max_wait: Optional[float] = 1.0):
+        self.stdin = stdin or sys.stdin.buffer
+        self.max_wait = max_wait or 1.0
+        r, w = os.pipe()
+        self._rpipe = os.fdopen(r, 'rb')
+        self._wpipe = os.fdopen(w, 'wb')
 
     def setup(self) -> None:
-        def handle_signal(signum, frame=None):
-            self.signals_queue.put_nowait(Signal(signum, None, time.monotonic(), datetime.now(timezone.utc)))
-
-        for s in set(signal.Signals) - {signal.SIGKILL, signal.SIGSTOP, signal.SIGINT}:
-            try:
-                signal.signal(s, handle_signal)
-            except ValueError:
-                pass
+        """
+        Performs any setup necessary for the console.
+        """
+        pass
 
     def close(self) -> None:
         """
-        Close the console.
+        Performs any cleanup necessary for the console.
         """
-        self.signals_queue.put(None)
+        if self._wpipe is not None:
+            self._wpipe.close()
+            self._wpipe = None
 
     def __enter__(self) -> 'Console':
         self.setup()
@@ -56,22 +43,20 @@ class Console(ABC):
     def __exit__(self, exc_type, exc_value, traceback) -> None:
         self.close()
 
-    def get_signal(self) -> Optional[Signal]:
+    def wait(self) -> None:
         """
-        Gets an enqueued signal from the console.
+        Wait for the console to have data available.
 
         Raises:
+            - `TimeoutError` if the console is not ready within the timeout.
             - `EOFError` if the console is closed.
         """
-        try:
-            s = self.signals_queue.get(timeout=self.pause)
-        except queue.Empty:
-            return None
+        rlist, _, _ = select.select([self.stdin, self._rpipe], [], [], self.max_wait)
+        if not rlist:
+            raise TimeoutError
 
-        if s is None:
+        if self._rpipe in rlist:
             raise EOFError
-
-        return s
 
     @abstractmethod
     def read(self) -> bytes:
@@ -79,179 +64,83 @@ class Console(ABC):
         Read data from the console.
 
         Raises:
-            - `EOFError` if the console is closed.
+            - `EOFError` if the console is closed while reading.
         """
         raise NotImplementedError
 
     @property
     def tty_size(self) -> Optional[Tuple[int, int]]:
         """
-        The size of the TTY.
+        The size of the TTY, if any.
+
+        Raises:
+            - `NotImplementedError` if the TTY size cannot be determined.
         """
-        return None
-
-
-class NullConsole(Console):
-    """
-    A console implementation that immediately raises `EOFError`.
-    """
-
-    def read(self) -> bytes:
-        raise EOFError
-
-
-class InterruptibleConsole(Console):
-    """
-    A console implementation that can be interrupted.
-    """
-
-    def __init__(self, stdin: Optional[BinaryIO] = None, pause: Optional[float] = None):
-        super().__init__(pause)
-        self.stdin: BinaryIO = stdin or sys.stdin.buffer
-        r, w = os.pipe()
-        self._rpipe = os.fdopen(r, 'rb', 0)
-        self._wpipe = os.fdopen(w, 'wb', 0)
-
-    def close(self) -> None:
-        super().close()
-        if self._wpipe is not None:
-            self._wpipe.close()
-            self._wpipe = None
-
-    def wait(self, pause: Optional[float] = None) -> bool:
-        pause = pause if pause is not None else self.pause
-        if self._wpipe is None:
-            raise EOFError
-
-        readables, _, _ = select.select([self.stdin, self._rpipe], [], [], pause)
-        if not readables:
-            if self._wpipe is None:
-                raise EOFError
-            return False
-
-        if self._rpipe in readables:
-            self._rpipe.read()
-            raise EOFError
-
-        return True
-
-    def read(self) -> bytes:
-        if not self.wait():
-            return b''
-
-        return self.read_now()
-
-    @abstractmethod
-    def read_now(self) -> bytes:
         raise NotImplementedError
 
 
-class LineConsole(InterruptibleConsole):
+class LineConsole(Console):
     """
-    A console implementation that reads line-by-line.
+    A console implementation that reads from a line-based input.
     """
-
-    def read_now(self) -> bytes:
-        line = self.stdin.readline()
-        if not line:
-            raise EOFError
-        return line
-
-
-class ChunkedConsole(InterruptibleConsole):
-    """
-    A console implementation that reads in chunks.
-    """
-
-    def __init__(self, stdin: Optional[BinaryIO] = None, bufsize: Optional[int] = None, pause: Optional[float] = None):
-        super().__init__(stdin, pause)
-        self._bufsize = bufsize or 4096
-
-    def read_now(self) -> bytes:
-        data = self.stdin.read(self._bufsize)
-        if not data:
-            raise EOFError
-        return data
-
-
-class TTYConsole(ChunkedConsole):
-    """
-    A console implementation that reads from a TTY.
-    """
-
-    def __init__(self, stdin: Optional[BinaryIO] = None, bufsize: Optional[int] = None, pause: Optional[float] = None):
-        super().__init__(stdin, bufsize, pause or 0.2)
-        self._old_attrs = None
-        self._old_blocking = None
-        self._eof = False
-        self._interrupted = False
-
-    def setup(self) -> None:
-        super().setup()
-        self._old_blocking = os.get_blocking(self.stdin.fileno())
-        self._old_attrs = termios.tcgetattr(self.stdin.fileno())
-        tty.setraw(self.stdin.fileno())
-        os.set_blocking(self.stdin.fileno(), False)
-
-    def close(self) -> None:
-        try:
-            super().close()
-        finally:
-            if self._old_blocking is not None:
-                os.set_blocking(self.stdin.fileno(), self._old_blocking)
-                self._old_blocking = None
-            if self._old_attrs is not None:
-                termios.tcsetattr(self.stdin.fileno(), termios.TCSADRAIN, self._old_attrs)
-                self._old_attrs = None
 
     @property
     def tty_size(self) -> Tuple[int, int]:
+        """
+        TTY size determined by the file descriptor of the stdin.
+
+        Raises:
+            - `NotImplementedError` if the stdin is not a TTY.
+        """
+        if not self.stdin.isatty():
+            raise NotImplementedError('stdin is not a TTY')
+
         s = struct.pack("HHHH", 0, 0, 0, 0)
         rows, cols, _, _ = struct.unpack("HHHH", fcntl.ioctl(self.stdin.fileno(), termios.TIOCGWINSZ, s))
         return rows, cols
 
-    def get_signal(self) -> Optional[Signal]:
-        s = super().get_signal()
-        if s is None:
-            return s
-
-        if int(s.signum) == signal.SIGWINCH:
-            s = s._replace(tty_size=self.tty_size)
-
-        return s
-
     def read(self) -> bytes:
-        if self._interrupted:
-            raise KeyboardInterrupt
+        """
+        Read data from the console.
 
-        return super().read()
+        Raises:
+            - `EOFError` if the console is closed.
+        """
+        try:
+            self.wait()
+        except TimeoutError:
+            return b""
 
-    def read_now(self) -> bytes:
-        if self._eof:
+        data = self.stdin.readline()
+        if not data:
             raise EOFError
 
-        buffer = bytearray()
-        t0 = time.monotonic()
+        return data
 
-        while len(buffer) < self._bufsize and time.monotonic() - t0 < self.pause:
-            try:
-                b = self.stdin.read(1)
-            except BlockingIOError:
-                b = None
 
-            if b is None:
-                continue
-            elif b == b'\x03':
-                self._interrupted = True
-                self.signals_queue.put_nowait(Signal(signal.SIGINT, None, time.monotonic(), datetime.now(timezone.utc)))
-                break
-            elif not b:
-                self._eof = True
-                break
+class RawConsole(Console):
+    """
+    A console implementation that reads from a raw input.
+    """
 
-            buffer.extend(b)
+    def __init__(self, stdin: Optional[BinaryIO] = None, max_wait: Optional[float] = 1.0, bufsize: Optional[int] = 1024):
+        super().__init__(stdin, max_wait)
+        self.bufsize = bufsize or 1024
 
-        return bytes(buffer)
+    def read(self) -> bytes:
+        """
+        Read data from the console.
+        """
+        try:
+            self.wait()
+        except TimeoutError:
+            return b""
+
+        data = self.stdin.read(self.bufsize)
+        if not data:
+            raise EOFError
+
+        return data
 
 
 if __name__ == '__main__':
@@ -262,25 +151,32 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     if sys.argv[1] == 'line':
-        console = LineConsole(pause=1)
+        console = LineConsole()
     elif sys.argv[1] == 'chunked':
         console = ChunkedConsole(pause=1, bufsize=5)
     elif sys.argv[1] == 'tty':
-        console = TTYConsole(pause=1, bufsize=5)
+        console = TTYConsole(max_wait=1, bufsize=5)
     else:
         console = NullConsole()
 
-    def stop():
-        time.sleep(5.0)
+    # def stop():
+    #     time.sleep(5.0)
+    #     logging.info('closing console')
+    #     console.close()
+    #     logging.info('closed console')
+
+    def on_signal(signum, frame):
+        logging.info('signal %s', signum)
         logging.info('closing console')
         console.close()
         logging.info('closed console')
 
-    threading.Thread(target=stop, daemon=True).start()
+    signal.signal(signal.SIGINT, on_signal)
+    # threading.Thread(target=stop, daemon=True).start()
 
     with console:
+        logging.info('reading with %s', console)
         while True:
-            logging.info('reading with %s', console)
             try:
                 line = console.read()
             except EOFError:
---
+++ test/executor/test_console.py
@@ -0,0 +1,98 @@
+from ast import Call
+from contextlib import contextmanager
+from pathlib import Path
+import os
+import threading
+import time
+from typing import BinaryIO, Callable, Iterator
+
+import pytest
+
+from livy_uploads.executor.console import LineConsole, RawConsole
+
+
+
+class TestLineConsole:
+    def test_file(self, tmp_path: Path):
+        file = tmp_path / 'test.txt'
+        file.write_text('line 1\nline 2\nline 3\n')
+
+        console = LineConsole(stdin=file.open('rb'))
+
+        assert console.read() == b'line 1\n'
+        assert console.read() == b'line 2\n'
+        assert console.read() == b'line 3\n'
+        with pytest.raises(EOFError):
+            console.read()
+
+    def test_pipe(self):
+        r, w = os.pipe()
+        rpipe = os.fdopen(r, 'rb')
+        wpipe = os.fdopen(w, 'wb')
+
+        console = LineConsole(stdin=rpipe, max_wait=1.0)
+
+        t0 = time.monotonic()
+        assert console.read() == b''
+        assert time.monotonic() - t0 >= 1.0
+
+        delayed_write(wpipe, 0.3, b'line 1\n')
+        t0 = time.monotonic()
+        assert console.read() == b'line 1\n'
+        assert time.monotonic() - t0 >= 0.3
+
+        delay(0.1, wpipe.close)
+        t0 = time.monotonic()
+        with pytest.raises(EOFError):
+            console.read()
+        assert time.monotonic() - t0 >= 0.1
+
+
+class TestRawConsole:
+    def test_file(self, tmp_path: Path):
+        file = tmp_path / 'test.txt'
+        file.write_text('1\n2\n3\n')
+
+        console = RawConsole(stdin=file.open('rb'), bufsize=2)
+
+        assert console.read() == b'1\n'
+        assert console.read() == b'2\n'
+        assert console.read() == b'3\n'
+        with pytest.raises(EOFError):
+            console.read()
+
+    def test_pipe(self):
+        r, w = os.pipe()
+        rpipe = os.fdopen(r, 'rb')
+        wpipe = os.fdopen(w, 'wb')
+
+        console = RawConsole(stdin=rpipe, max_wait=1.0, bufsize=2)
+
+        t0 = time.monotonic()
+        assert console.read() == b''
+        assert time.monotonic() - t0 >= 1.0
+
+        delayed_write(wpipe, 0.3, b'1\n')
+
+        t0 = time.monotonic()
+        assert console.read() == b'1\n'
+        assert time.monotonic() - t0 >= 0.3
+
+        delay(0.1, wpipe.close)
+
+        t0 = time.monotonic()
+        with pytest.raises(EOFError):
+            console.read()
+        assert time.monotonic() - t0 >= 0.1
+
+
+def delay(dt: float, func: Callable):
+    def run():
+        time.sleep(dt)
+        func()
+
+    thread = threading.Thread(target=run, daemon=True)
+    thread.start()
+
+def delayed_write(fp: BinaryIO, dt: float, data: bytes):
+    delay(dt, lambda: [fp.write(data), fp.flush()])
